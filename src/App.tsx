53d15317a39b0209948f9755887b48a440a00266
Nelli Gaifullina <nellie.mailbox@gmail.com>
2021-09-21 00:14:06 +0300

    copied counter, renamed to events

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Events } from "./features/events/Events";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <Events />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
